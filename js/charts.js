701989611dbed346445dfed79668a16dcfd92b9f
Sevda Pruitt <sevda9868@gmail.com>
2022-03-13 11:31:54 -0500

    js file updatea

--- js/charts.js
+++ js/charts.js
@@ -48,3 +48,126 @@ function buildMetadata(sample) {
     });
   });
 }
+
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+    var sampleArray = data.samples
+    var metaArray = data.metadata
+
+    //  4. Create a variable that filters the samples for the object with the desired sample number.
+    //  5. Create a variable that holds the first sample in the array.
+    var filterSample = sampleArray.filter(sampleObject => sampleObject.id == sample)[0];
+    
+    //-------------//
+    // Gauge //
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metadataArray = metaArray.filter(sampleObject => sampleObject.id == sample)[0];
+    // console.log(metadataArray);
+    //-------------//
+    
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_Ids = filterSample.otu_ids;
+    var otu_labels = filterSample.otu_labels;
+    var sampleValues = filterSample.sample_values;
+
+    //-------------//
+    // Gauge //
+    // 3. Create a variable that holds the washing frequency.
+    var washfreq = metadataArray.wfreq;
+    //-------------//
+
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    // so the otu_ids with the most bacteria are last. 
+  
+    var yticks = otu_Ids.slice(0, 10).map(out_Ids => `OTU ${out_Ids}`).reverse();
+
+    // // 8. Create the trace for the bar chart. 
+    var barData = [{
+      x : sampleValues.slice(0, 10).reverse(),
+      y : yticks,
+      type : "bar",
+      text : otu_labels.slice(0, 10).reverse(),
+      orientation : "h" 
+    }];
+
+    // // 9. Create the layout for the bar chart. 
+    var barLayout = {
+     title: "<b>Top 10 Bacteria Cultures Found<b>",
+     font: {
+       family: 'Courier New, monospace',
+       size: 14
+     }
+    };
+
+    // // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout, {displayModeBar: false});
+
+    // Bubble chart
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x : otu_Ids,
+      y : sampleValues,
+      text : otu_labels,
+      mode : "markers",
+      marker : {
+        size : sampleValues,
+        color : otu_Ids,
+        colorscale : "Earth"
+      }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "<b>Bacteria Species per Sample<b>",
+      xaxis: {title: "OTU ID"},
+      hovermode: "closest",
+      font: {
+        family: 'Courier New, monospace',
+        size: 18
+      }
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout, {scrollZoom: true}, {displayModeBar: false}); 
+    
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        value: washfreq,
+        title: { text: "<b>Belly Button Washing Frequency<b>" 
+          + "<br>Scrubs per week" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          bar: {color: "black"},
+          axis: {range: [null, 10]},
+          steps: [
+            {range: [0, 2], color: "rgb(168,182,195)"},
+            {range: [2, 4], color: "rgb(169,221,241)"},
+            {range: [4, 6], color: "rgb(96,196,237)"},
+            {range: [6, 8], color: "lrgb(54,164,237)"},
+            {range: [8, 10], color: "grgb(12,60,140)"}]
+        }
+      }
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 600, 
+      height: 500, 
+      margin: { t: 0, b: 0 },
+      font: {
+        family: 'Courier New, monospace',
+        size: 25
+      }
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout, {displayModeBar: false});
+  });
+}
